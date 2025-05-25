f1a8bb1f21c349bebff6053f331bfcfcbfe76d24
Aimlessss <katharva2002@gmail.com>
2025-05-25 14:34:00 +0000

    config

--- tempConfig.ts
+++ tempConfig.ts
@@ -1,7 +1,7 @@
-export const apiKey = process.env.ZERODHA_API_KEY ?? 'ev7o6c03k1fdwrjk';
+export const apiKey = process.env.ZERODHA_API_KEY ?? '';
 export const symbol = process.env.SYMBOL ?? "JPPOWER";
 export const gapThresholdPercentage = process.env.GAP_THRESHOLD_PERCENT ?? 1.0; // Default to 2% if not set
-export const apiSecret = 'gmin9zgivnbrflom8qocimzdn34dais9';
+export const apiSecret = '';
 export const quantity = parseInt(process.env.QUANTITY ?? "1");
 export const stopLoss = parseInt(process.env.STOPLOSS ?? "0");
 
